3e214958efb71d61f74d49fb89f8a77ccd0849e7
SanjeevkMishra <sanjeevroy56@gmail.com>
2025-09-07 13:03:14 +0530

    Majority Problem solved using HashTable or Dictionary

---
+++ HashTable/MajorityElement.cs
@@ -0,0 +1,67 @@
+﻿//169.Majority Element
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Given an array nums of size n, return the majority element.
+
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+
+//Example 1:
+
+//Input: nums = [3, 2, 3]
+//Output: 3
+//Example 2:
+
+//Input: nums = [2, 2, 1, 1, 1, 2, 2]
+//Output: 2
+
+
+
+//Constraints:
+
+//n == nums.length
+//1 <= n <= 5 * 104
+//- 109 <= nums[i] <= 109
+
+
+
+//Follow - up: Could you solve the problem in linear time and in O(1) space ?
+
+//Solution: Using Hash Table or Dictionary
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        // use Hash Table(Dictionary)
+        Dictionary<int, int> myDict = new Dictionary<int, int>();
+        int max = 0;
+        int maxNum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (myDict.ContainsKey(nums[i]))
+            {
+                myDict[nums[i]] += 1;
+            }
+            else
+            {
+                myDict.Add(nums[i], 1);
+            }
+        }
+
+        foreach (KeyValuePair<int, int> kvp in myDict)
+        {
+            if (kvp.Value > max)
+            {
+                max = kvp.Value;
+                maxNum = kvp.Key;
+            }
+        }
+
+        return maxNum;
+    }
+}
